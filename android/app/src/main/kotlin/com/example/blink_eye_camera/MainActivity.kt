053e0d6393bedc2ffc9d29f3340537929819f285
MIS <manthanshalini48@gmail.com>
2024-08-17 17:44:21 +0530

    first

---
+++ android/app/src/main/kotlin/com/example/blink_eye_camera/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.blink_eye_camera
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'example_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: M7ExpampleScreen(),
+    );
+  }
+}
